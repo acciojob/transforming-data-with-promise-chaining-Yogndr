fd210f32311579f8d3b55a677f55b307887c813e
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-05-31 13:15:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 //your JS code here. If required.
+const input=document.querySelector("#ip");
+const button=document.querySelector("#btn");
+const output=document.querySelector("#output");
+button.addEventListener("click",()={
+	const val=parseInt(input.value);
+	return new Promise((resolve)=>{
+
+		setTimeout(()=>{
+			output.textContent=`Result: ${val}`
+			resolve(val);
+		},2000)
+		
+	}).
+		then((val)=>{
+			return new Promise((resolve)=>{
+				setTimeout(()=>{
+					val=val*2;
+					output.textContent=`Result: ${val}`
+			        resolve(val);
+				},2000)
+			})
+			
+		}).
+		then((val)=>{
+			return new Promise((resolve)=>{
+				setTimeout(()=>{
+					val=val-3;
+					output.textContent=`Result: ${val}`
+			        resolve(val);
+				},1000)
+			})
+			
+		}).
+         then((val)=>{
+			return new Promise((resolve)=>{
+				setTimeout(()=>{
+					val=val/2;
+					output.textContent=`Result: ${val}`
+			        resolve(val);
+				},1000)
+			})
+			
+		}).
+          then((val)=>{
+			return new Promise((resolve)=>{
+				setTimeout(()=>{
+					val=val+10;
+					output.textContent=`Final Result: ${val}`
+			        resolve(val);
+				},1000)
+			})
+			
+		})
+		
+})
